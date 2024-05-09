8fb0f9299946dd325304afd29e3bf6635041b386
itsquelc <raquelnunescirino@gmail.com>
2024-05-09 11:04:08 -0300

    estilizando site

--- quelloShopList/src/app/home/index.js
+++ quelloShopList/src/app/home/index.js
@@ -7,16 +7,34 @@ ImageBackground,
 TextInput,
 TouchableOpacity
 } from 'react-native'
-import {Ionicons} from "expo/vector-icons"; 
+import {Ionicons} from "@expo/vector-icons"; 
+import { styles } from './style';
 
 export default function Home() {
   return (
-    <SafeAreaView style={{flex: 1 }}>
-      <ImageBackground  source={require('../../assets/background.jpg')}
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground 
+        source={require('../../assets/background.jpg')}
         resizeMode='repeat'
         style={{ flex: 1, justifyContent: 'flex-start'}}
         >
+          <View style={styles.header}>
+          <Text style={styles.title}>Lista de Produtos</Text>
+          <View>
+            <Ionicons name="trash" size={32} color="#fff" />
+          </View>
+          </View>
 
+          {/* Lista de Produtos */}
+
+          <View style={styles.footer}>
+          <View style={styles.inputContainer}>
+            <TextInput />
+          </View>
+          <TouchableOpacity style={styles.iconContainer}>
+            <Ionicons name="add" size={36} color="#fff" />
+          </TouchableOpacity>
+          </View>
       </ImageBackground>
     </SafeAreaView>
   )
