881df9a59e6da6de948921195aad8dc58abccbd1
itsquelc <raquelnunescirino@gmail.com>
2024-05-16 11:38:47 -0300

    visual do site

--- quelloShopList/src/app/home/index.js
+++ quelloShopList/src/app/home/index.js
@@ -9,6 +9,7 @@ TouchableOpacity
 } from 'react-native'
 import {Ionicons} from "@expo/vector-icons"; 
 import { styles } from './style';
+import ItemList from '../../components/ItemList';
 
 export default function Home() {
   return (
@@ -26,10 +27,18 @@ export default function Home() {
           </View>
 
           {/* Lista de Produtos */}
+          <ItemList item={"Café"}></ItemList>
+          <ItemList item={"Chocolate"}></ItemList>
+
 
           <View style={styles.footer}>
           <View style={styles.inputContainer}>
-            <TextInput />
+            <TextInput
+            color="#fff"
+            fontSize={18}
+            placeholderTextColor="#aeaeae"
+            placeholder='Digite o nome do produto...'
+            />
           </View>
           <TouchableOpacity style={styles.iconContainer}>
             <Ionicons name="add" size={36} color="#fff" />
